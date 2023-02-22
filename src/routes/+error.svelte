40b3eabec9dd20e15ef07822ec27cd6c225c1fc0
Adalan <sunymon@protonmail.com>
2023-02-21 19:45:25 -0600

    add some cool stuff

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import Heading from "$lib/component/Typography/Heading.svelte";
+</script>
+
+<Heading>
+	<svelte:fragment slot="heading">{$page.status}</svelte:fragment>
+	<svelte:fragment slot="sub-heading">{$page.error?.message}</svelte:fragment>
+</Heading>
