2acccb6a97ad66e03cb32854a6d6bc48bfb6ecd3
Adalan <sunymon@protonmail.com>
2023-02-21 20:11:03 -0600

    Commit: Add CheckBoxInput component and update +page route

---
+++ src/lib/components/Input/CheckBoxInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let checked: boolean;
+	export let name: string = '';
+	export let label: string = '';
+</script>
+
+<fieldset>
+	<label for={name} class="flex items-center gap-2 text-zinc-400 pl-4">
+		<input bind:checked type="checkbox" {name} id={name} class="bg-zinc-900 border-zinc-800 transition-all" />
+		{label}
+	</label>
+</fieldset>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-	import QRCode from "qrcode";
-	import { fade } from "svelte/transition";
-	import { addNewWifi } from "$lib/store/wifiList";
-	import Heading from "$lib/component/Typography/Heading.svelte";
-	import TextInput from "$lib/component/Input/TextInput.svelte";
-	import RadioGroup from "$lib/component/Input/RadioGroupInput.svelte";
-	import PasswordInput from "$lib/component/Input/PasswordInput.svelte";
-	import CheckBoxInput from "$lib/component/Input/CheckBoxInput.svelte";
-	import QRCodePreviewModal from "$lib/component/Modal/QRCodePreviewModal.svelte";
-	import Toast from "$lib/component/Toast.svelte";
-	let ssid = "";
-	let encryption: "WPA" | "WEP" | "nopass" = "WPA";
-	let password = "";
+	import QRCode from 'qrcode';
+	import { fade } from 'svelte/transition';
+	import { addNewWifi } from '$lib/stores/wifiList';
+	import Heading from '$lib/components/Typography/Heading.svelte';
+	import TextInput from '$lib/components/Input/TextInput.svelte';
+	import RadioGroup from '$lib/components/Input/RadioGroupInput.svelte';
+	import PasswordInput from '$lib/components/Input/PasswordInput.svelte';
+	import CheckBoxInput from '$lib/components/Input/CheckBoxInput.svelte';
+	import QRCodePreviewModal from '$lib/components/Modal/QRCodePreviewModal.svelte';
+	import Toast from '$lib/components/Toast.svelte';
+	let ssid = '';
+	let encryption: 'WPA' | 'WEP' | 'nopass' = 'WPA';
+	let password = '';
 	let hidden = false;
-	let qrSrc = "";
+	let qrSrc = '';
 	let isQRPreviewShow = false;
 	let isToastShow = false;
-	$: isPasswordFieldUnavailable = encryption === "nopass";
+	$: isPasswordFieldUnavailable = encryption === 'nopass';
 	const formatToQR = () => {
 		const encryptionString = `T:${encryption};`;
 		const ssidString = `S:${ssid};`;
-		const paswordString = password ? `P:${password};` : "";
-		const hiddenString = hidden ? `H:${hidden};` : "";
+		const paswordString = password ? `P:${password};` : '';
+		const hiddenString = hidden ? `H:${hidden};` : '';
 		const wifiInfoString = `WIFI:${encryptionString}${ssidString}${paswordString}${hiddenString};`;
 		QRCode.toDataURL(wifiInfoString, { scale: 50, margin: 3 }, (err, url) => {
 			if (err) console.error(err);
@@ -39,10 +39,7 @@
 	<title>Create | Wifi2QR</title>
 </svelte:head>
 
-<Heading
-	heading="Create"
-	subHeading="Your wifi information stays in your machine and did not send to any server."
-/>
+<Heading heading="Create" subHeading="Your wifi information stays in your machine and did not send to any server." />
 
 <div class="">
 	<form on:submit|preventDefault={handleFormSubmit} class="space-y-4">
@@ -52,16 +49,16 @@
 			bind:group={encryption}
 			legend="Encryption"
 			values={[
-				{ label: "WPA/WPA2/WPA3", name: "wpa", value: "WPA" },
-				{ label: "WEP", name: "wep", value: "WEP" },
-				{ label: "None", name: "nopass", value: "nopass" }
+				{ label: 'WPA/WPA2/WPA3', name: 'wpa', value: 'WPA' },
+				{ label: 'WEP', name: 'wep', value: 'WEP' },
+				{ label: 'None', name: 'nopass', value: 'nopass' }
 			]}
 		/>
 
 		<PasswordInput
 			bind:value={password}
 			disabled={isPasswordFieldUnavailable}
-			required={encryption !== "nopass"}
+			required={encryption !== 'nopass'}
 			label="Password"
 			name="password"
 			placeholder="Your wifi password"
@@ -70,9 +67,7 @@
 		<CheckBoxInput bind:checked={hidden} name="hidden" label="Hidden" />
 
 		<div class="flex items-center gap-2 pl-4">
-			<button
-				type="submit"
-				class="bg-blue-600 hover:opacity-80 active:opacity-90 px-6 py-2 transition-all"
+			<button type="submit" class="bg-blue-600 hover:opacity-80 active:opacity-90 px-6 py-2 transition-all"
 				>Create</button
 			>
 			{#if qrSrc}
@@ -80,7 +75,7 @@
 					transition:fade|local
 					class="bg-zinc-800 py-2 px-6 hover:opacity-80 active:opacity-90 transition-all"
 					on:click|preventDefault={() => (isQRPreviewShow = !isQRPreviewShow)}
-					>{isQRPreviewShow ? "Hide QR code" : "Show QR code"}</button
+					>{isQRPreviewShow ? 'Hide QR code' : 'Show QR code'}</button
 				>
 			{/if}
 		</div>
